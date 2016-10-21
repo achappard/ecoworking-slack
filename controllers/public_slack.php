c4bdf9669d63e3e8cf110c4897417d9bd54b9b32
Aurélien Chappard <aurelien.chappard@deefuse.fr>
2016-10-21 15:37:39 +0200

    Add gmap localisation

--- controllers/public_slack.php
+++ controllers/public_slack.php
@@ -21,7 +21,6 @@ class Public_slack extends MY_Controller {
     * Where do we eat today ?
     */
     public function onmangeou() {
-
         $token = $this->input->post('token');
         if( $token == $this->config->item('slack_token_onmangeou') ){
 
@@ -30,14 +29,38 @@ class Public_slack extends MY_Controller {
             $message =  $this->mdl_slack->getRandomMessage();
 
             header('Content-Type: application/json');
+
+            // Generates the name of the restaurant with (or not) the link to the website
+            $randomName = $restaurant['name'];
+
+            if( isset( $restaurant['url'] ) && !empty($restaurant['url']) ){
+                $restaurant_name = "*<" . $restaurant['url'] . "|" . $randomName . ">*";
+            }else{
+                $restaurant_name = "*" . $randomName . "*";
+            }
+            $slack_str = sprintf($message, $restaurant_name);
+
+            // Generate google maps preview
+            if ( isset($restaurant['address']) ){
+                $slack_str .= "\n";
+                $slack_str .= "<";
+                $restaurantPinLabel = substr( ucfirst( $restaurant['name'] ),0,1 );
+                $gmaps_url = "http://maps.googleapis.com/maps/api/staticmap?center=place%20des%20terreaux,%2069001%20Lyon&zoom=17&size=800x600&markers=color:red%7Clabel:" . $restaurantPinLabel . "%7C" . urlencode($restaurant['address']) . "&markers=color:0x67AC23%7Clabel:E%7C27%20rue%20romarin,%2069001%20Lyon";
+                $gmaps_url .= "&key=".$this->config->item('gmaps_slack_key');
+                $slack_str .= $gmaps_url;
+                $slack_str .= "| >";
+                $slack_str .= "(<https://www.google.com/maps/place/" . urlencode($restaurant['address']) . "|Voir sur Google Maps>)";
+            }
+
+
             $json = array(
+                "mrkdwn"        => true,
                 "response_type" => "in_channel",
-                "text"          => sprintf($message, "*" . $restaurant . "*")
+                "text"          => $slack_str
             );
             echo json_encode($json);
         }else{
             return;
         }
     }
-    
 }
--- models/mdl_slack.php
+++ models/mdl_slack.php
@@ -4,40 +4,151 @@ exit('No direct script access allowed');
 
 class Mdl_slack extends CI_Model {
 
-    public static $restaurants_list = array(
-        'Doma',
-        'Yaffa',
-        'Picard',
-        'La boulangerie Kaiser',
-        'Best Bagels',
-        'Adonys',
-        'Tralala',
-        'Komé',
-        'Comptoir du poulet',
-        'La Martinière',
-        'Crok\'n Roll',
-        'Konditori',
-        'Yomogi',
-        'Bangkok Royal',
-        'Gourmix',
-        'Le comptoir du poulet',
-        'Yabio',
-        'Casa Lola',
-        'Focaccia',
-        'Wazza',
-        'Athina',
-        'Edo Sushi',
-        'Gourmix',
-        'Cocoo Thai',
-        'Croc\'o Green',
-        'La Marsa',
-        'Frite Alors',
-        'La bicycletterie',
-        'Chez Jules',
-        'L\'indien à côté du Sun',
+    /**
+     * @todo : restaurant in a Yaml file
+     * @var array
+     */
+    private static $restaurants_list = array(
+        array(
+            'name'      =>  'Doma',
+            'address'   =>  '11 Rue Lanterne, 69001 Lyon',
+            'url'       =>  'http://www.domalyon.fr/'
+        ),
+        array(
+            'name'      =>  'Yaffa',
+            'address'   =>  '17 rue d\'Algérie, 69001 LYON',
+            'url'       =>  'http://www.yaafa.fr/'
+        ),
+        array(
+            'name'      =>  'Picard',
+            'address'   =>  '18 rue Constantine, 69001 LYON',
+            'url'       =>  'http://www.picard.fr/',
+        ),
+        array(
+            'name'      =>  'La boulangerie Kaiser',
+            'address'   =>  '15 Place Louis Pradel, 69001 Lyon',
+            'url'       =>  'http://www.maison-kayser.com/fr/',
+        ),
+        array(
+            'name'      =>  'Best Bagels',
+            'address'   =>  '1 Place Tobie Robatel, 69001 Lyon',
+            'url'       =>  'http://www.bestbagels.fr/',
+        ),
+        array(
+            'name'      =>  'Adonys',
+            'address'   =>  '13 Rue Puits Gaillot, 69001 Lyon',
+            'url'       =>  'http://www.petitpaume.com/etablissement/adonys.htm',
+        ),
+        array(
+            'name'      =>  'Tralala',
+            'address'   =>  '8 Rue Saint-Polycarpe, 69001 Lyon',
+            'url'       =>  'http://www.petitpaume.com/etablissement/tralala.htm',
+        ),
+        array(
+            'name'      =>  'Komé',
+            'address'   =>  '4 Rue Lanterne, 69001 Lyon',
+            'url'       =>  'http://www.sushikome.fr/',
+        ),
+        array(
+            'name'      =>  'Le Comptoir du poulet',
+            'address'   =>  '14 Rue Constantine, 69001 Lyon',
+            'url'       =>  'https://www.facebook.com/lecomptoirdupoulet/',
+        ),
+        array(
+            'name'      =>  'Boulangerie de la Martinière',
+            'address'   =>  '24 Rue de la Martinière, 69001 Lyon',
+            'url'       =>  'http://www.petitpaume.com/etablissement/la-boulangerie-de-la-martiniere.htm',
+        ),
+        array(
+            'name'      =>  'Crok\'n Roll',
+            'address'   =>  '1 Rue Désirée, 69001 Lyon',
+            'url'       =>  'http://www.thecrocknroll.fr',
+        ),
+        array(
+            'name'      =>  'Konditori',
+            'address'   =>  '9 rue des Capucins, 69001 Lyon',
+            'url'       =>  'http://www.konditori.fr/',
+        ),
+        array(
+            'name'      =>  'Yomogi',
+            'address'   =>  '1 Rue Hippolyte Flandrin, 69001 Lyon',
+            'url'       =>  'http://www.yomogilyon.com/',
+        ),
+        array(
+            'name'      =>  'Bangkok Royal',
+            'address'   =>  '40 Rue Sergent Blandan, 69001 Lyon',
+            'url'       =>  'http://www.bangkok-royal.fr/',
+        ),
+        array(
+            'name'      =>  'Gourmix',
+            'address'   =>  '22 Rue de la Platière, 69001 Lyon',
+            'url'       =>  'http://www.gourmix.fr/',
+        ),
+        array(
+            'name'      =>  'Yabio',
+            'address'   =>  '19 Rue du Garet, 69001 Lyon',
+            'url'       =>  'http://yabio.fr/',
+        ),
+        array(
+            'name'      =>  'La Focaccia',
+            'address'   =>  '6 Rue Désirée, 69001 Lyon',
+            'url'       =>  'http://www.pizzerialafocaccia.fr/',
+        ),
+        array(
+            'name'      =>  'Wazza',
+            'address'   =>  '17 Rue Désirée, 69001 Lyon',
+            'url'       =>  'http://www.wazza.pizza/',
+        ),
+        array(
+            'name'      =>  'Athina',
+            'address'   =>  '3 Rue Romarin, 69001 Lyon',
+            'url'       =>  'http://www.petitpaume.com/etablissement/athina.htm',
+        ),
+        array(
+            'name'      =>  'Edo Sushi',
+            'address'   =>  '9 Rue Sainte-Catherine, 69001 Lyon',
+            'url'       =>  'http://edolyon.fr/',
+        ),
+        array(
+            'name'      =>  'Cocoo Thai',
+            'address'   =>  '7 Rue Neuve, 69001 Lyon',
+            'url'       =>  'http://cocoo.fr/',
+        ),
+        array(
+            'name'      =>  'Croc\'o Green',
+            'address'   =>  '13 Rue Terraille, 69001 Lyon',
+            'url'       =>  'http://www.crocogreen.fr/',
+        ),
+        array(
+            'name'      =>  'La Marsa',
+            'address'   =>  '16 Rue d\'Algérie, 69001 Lyon',
+            'url'       =>  'http://www.restaurant-lamarsa.fr/',
+        ),
+        array(
+            'name'      =>  'Frite Alors!',
+            'address'   =>  '20 Rue Terme, 69001 Lyon',
+            'url'       =>  'http://fritealors.fr/',
+        ),
+        array(
+            'name'      =>  'La bicycletterie',
+            'address'   =>  '16 Rue Romarin, 69001 Lyon',
+            'url'       =>  'http://www.la-bicycletterie.com',
+        ),
+        array(
+            'name'      =>  'Chez Jules',
+            'address'   =>  '4, Rue Joseph Serlin, 69001 Lyon',
+            'url'       =>  'http://boulangeriechezjules.fr/',
+        ),
+        array(
+            'name'      =>  'Tandoori, l\'indien à côté du Sun',
+            'address'   =>  '8 Rue Sainte-Marie-des-Terreaux, 69001 Lyon',
+            'url'       =>  '',
+        ),
     );
 
-    public static $message = array(
+
+
+    private static $message = array(
         '%s bien sûr !',
         '%s, car ça fait longtemps non ?',
         '%s comme d\'hab non ?',
